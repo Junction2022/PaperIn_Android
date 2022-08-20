ae7ee65d96fd6b27713280e3ff1cee3374ab7cf2
임세현 <sh00710000@gmail.com>
2022-08-21 05:48:00 +0900

    style: bg, ic

---
+++ app/src/main/java/com/jammin/myapplication/utils/TierEnum.kt
@@ -0,0 +1,15 @@
+package com.jammin.myapplication.utils
+
+import com.jammin.myapplication.R
+
+enum class TierEnum(val layoutId: Int) {
+    IRON(R.drawable.bg_iron),
+    BRONZE(R.drawable.bg_bronze),
+    SILVER(R.drawable.bg_silver),
+    GOLD(R.drawable.bg_gold),
+    PLATINUM(R.drawable.bg_platinum),
+    DIAMOND(R.drawable.bg_diamond),
+    MASTER(R.drawable.bg_master),
+    GRANDMASTER(R.drawable.bg_grand_master),
+    CHALLENGER(R.drawable.bg_challenge)
+}
