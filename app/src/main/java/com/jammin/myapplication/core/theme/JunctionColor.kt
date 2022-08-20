52786d1522168b608d5f4d9c3464dd7a9a05e0e7
임세현 <sh00710000@gmail.com>
2022-08-21 05:48:51 +0900

    feat: tier color

--- app/src/main/java/com/jammin/myapplication/core/theme/JunctionColor.kt
+++ app/src/main/java/com/jammin/myapplication/core/theme/JunctionColor.kt
@@ -18,4 +18,17 @@ object JunctionColor {
 
     val Check = Color(0xFF04DF00)
     val Error = Color(0xFFDC3035)
+
+    object Tier {
+
+        val Iron = Color(0xFF8E8C84)
+        val Bronze = Color(0xFF976F56)
+        val Silver = Color(0xFFD4E1EC)
+        val Gold = Color(0xFFEEDF89)
+        val Platinum = Color(0xFFB6E1EB)
+        val Diamond = Color(0xFFB9C6E0)
+        val Master = Color(0xFFAB61ED)
+        val GrandMaster = Color(0xFFE4674D)
+        val Challenger = Color(0xFF70BDE9)
+    }
 }
