564d8d7ea3c887b844f501c8736de6070151437d
임세현 <sh00710000@gmail.com>
2022-08-21 07:03:25 +0900

    feat: evaluation vm

---
+++ app/src/main/java/com/jammin/myapplication/feature/evaluation/vm/EvaluationVM.kt
@@ -0,0 +1,17 @@
+package com.jammin.myapplication.feature.evaluation.vm
+
+import androidx.lifecycle.ViewModel
+import com.jammin.myapplication.feature.evaluation.mvi.EvaluationSideEffect
+import com.jammin.myapplication.feature.evaluation.mvi.EvaluationState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
+import javax.inject.Inject
+
+@HiltViewModel
+class EvaluationVM @Inject constructor(
+
+) : ContainerHost<EvaluationState, EvaluationSideEffect>, ViewModel() {
+
+    override val container = container<EvaluationState, EvaluationSideEffect>(EvaluationState())
+}
