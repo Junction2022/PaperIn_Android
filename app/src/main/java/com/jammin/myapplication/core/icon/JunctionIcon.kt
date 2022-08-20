c31404122350da77673af96ab6d963abf03dd50e
임세현 <sh00710000@gmail.com>
2022-08-20 20:35:44 +0900

    style: icon

---
+++ app/src/main/java/com/jammin/myapplication/core/icon/JunctionIcon.kt
@@ -0,0 +1,20 @@
+package com.jammin.myapplication.core.icon
+
+import com.jammin.myapplication.R
+
+object JunctionIcon {
+
+    fun Password(visible: Boolean) =
+        if (visible) R.drawable.ic_password_visible else R.drawable.ic_password_invisible
+
+    fun Good(state: Boolean) =
+        if (state) R.drawable.ic_good_on else R.drawable.ic_good_off
+
+    val Cancel = R.drawable.ic_cancel
+
+    val File = R.drawable.ic_file_off
+
+    val Message = R.drawable.ic_message
+
+    val Back = R.drawable.ic_back_small
+}
