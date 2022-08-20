011048a8de1af5f4241ad729f6332ad895acda31
임세현 <sh00710000@gmail.com>
2022-08-20 17:48:30 +0900

    chore: version & compose gradle

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -38,6 +38,6 @@ object Versions {
     const val HILT_NAV = "1.0.0"
 
     const val SPLASH_SCREEN_VERSION = "1.0.0-beta01"
-    const val COMPOSE = "1.2.0-alpha03"
-    const val ACTIVITY_COMPOSE = "1.4.0"
+    const val COMPOSE = "1.2.0-rc01"
+    const val ACTIVITY_COMPOSE = "1.6.0-alpha05"
 }
