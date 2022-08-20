06436de0c04e9a7b50c362bc89409bc4331897e0
임세현 <sh00710000@gmail.com>
2022-08-20 23:09:38 +0900

    fix: room & repository module

--- app/src/main/java/com/jammin/myapplication/di/module/RepositoryModule.kt
+++ app/src/main/java/com/jammin/myapplication/di/module/RepositoryModule.kt
@@ -1,16 +1,20 @@
 package com.jammin.myapplication.di.module
 
+import com.jammin.myapplication.data.repository.AcademicRepository
+import com.jammin.myapplication.data.repository.AcademicRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
-//    @Singleton
-//    @Binds
-//    abstract fun providesNoticeRepository(
-//        noticeRepositoryImpl: NoticeRepositoryImpl
-//    ): NoticeRepository
+    @Singleton
+    @Binds
+    abstract fun providesNoticeRepository(
+        academicRepositoryImpl: AcademicRepositoryImpl
+    ): AcademicRepository
 }
