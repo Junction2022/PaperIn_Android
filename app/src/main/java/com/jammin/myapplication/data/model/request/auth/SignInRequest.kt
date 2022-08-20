4d05e0ac4a7861ec3b0db147382bf1c8f8ddd763
최민재 <cmjcmj1230@gmail.com>
2022-08-21 06:05:14 +0900

    fix: request not correct

--- app/src/main/java/com/jammin/myapplication/data/model/request/auth/SignInRequest.kt
+++ app/src/main/java/com/jammin/myapplication/data/model/request/auth/SignInRequest.kt
@@ -2,5 +2,5 @@ package com.jammin.myapplication.data.model.request.auth
 
 data class SignInRequest(
     val id: String,
-    val nickname: String
+    val password: String
 )
--- app/src/main/java/com/jammin/myapplication/data/model/request/auth/SignUpRequest.kt
+++ app/src/main/java/com/jammin/myapplication/data/model/request/auth/SignUpRequest.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class SignUpRequest(
 
-    @field:SerializedName("id") val id: Int,
+    @field:SerializedName("id") val id: String,
     @field:SerializedName("password") val password: String,
     @field:SerializedName("nickname") val nickname: String
 )
