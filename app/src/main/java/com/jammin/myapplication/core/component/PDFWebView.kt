2f07071012294e4fa13c85aa86741959b17c6f91
임세현 <sh00710000@gmail.com>
2022-08-21 03:49:22 +0900

    fix: pdf null 허용 & null view

--- app/src/main/java/com/jammin/myapplication/core/component/PDFWebView.kt
+++ app/src/main/java/com/jammin/myapplication/core/component/PDFWebView.kt
@@ -6,13 +6,24 @@ import android.view.ViewGroup
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.viewinterop.AndroidView
+import com.jammin.myapplication.core.icon.JunctionIcon
+import com.jammin.myapplication.core.theme.JunctionColor
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
 fun PDFWebView(
-    url: String,
+    modifier: Modifier = Modifier,
+    url: String?,
     layoutParams: ViewGroup.LayoutParams = ViewGroup.LayoutParams(
         ViewGroup.LayoutParams.MATCH_PARENT,
         ViewGroup.LayoutParams.MATCH_PARENT
@@ -20,22 +31,39 @@ fun PDFWebView(
 ) {
     val mUrl = "http://docs.google.com/gview?embedded=true&url=$url"
 
-    AndroidView(factory = {
-        WebView(it).apply {
-            this.layoutParams = layoutParams
-            webViewClient = WebViewClient()
-            webChromeClient = WebChromeClient()
-            with(settings) {
-                setSupportZoom(true)
-                javaScriptEnabled = true
-                defaultTextEncodingName = Xml.Encoding.UTF_8.name
-                loadWithOverviewMode = true
-                useWideViewPort = true
-                builtInZoomControls = true
-            }
-            loadUrl(mUrl)
+    if (url != null) {
+        AndroidView(
+            modifier = modifier.fillMaxWidth(),
+            factory = {
+                WebView(it).apply {
+                    this.layoutParams = layoutParams
+                    webViewClient = WebViewClient()
+                    webChromeClient = WebChromeClient()
+                    with(settings) {
+                        setSupportZoom(true)
+                        javaScriptEnabled = true
+                        defaultTextEncodingName = Xml.Encoding.UTF_8.name
+                        loadWithOverviewMode = true
+                        useWideViewPort = true
+                        builtInZoomControls = true
+                    }
+                    loadUrl(mUrl)
+                }
+            },
+            update = {
+                it.loadUrl(mUrl)
+            })
+    } else {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = JunctionColor.Gray100),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                painter = painterResource(id = JunctionIcon.File),
+                contentDescription = null
+            )
         }
-    }, update = {
-        it.loadUrl(mUrl)
-    })
+    }
 }
